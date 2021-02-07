09489866eadf0d98d8f27ebfbbea331378fc09e5
Trevor Kramer <trevordkramer@gmail.com>
2021-02-07 13:41:57 -0600

    fixes

--- Code/MinimalLib/minilib.cpp
+++ Code/MinimalLib/minilib.cpp
@@ -22,76 +22,9 @@
 #include <GraphMol/Descriptors/MolDescriptors.h>
 #include <GraphMol/Fingerprints/MorganFingerprints.h>
 #include <GraphMol/Fingerprints/Fingerprints.h>
-#include <GraphMol/Depictor/RDDepictor.h>
-#include <GraphMol/CIPLabeler/CIPLabeler.h>
 #include <DataStructs/BitOps.h>
 
 using namespace RDKit;
-
-std::string process_details(const std::string &details, unsigned int &width,
-                            unsigned int &height, int &offsetx, int &offsety,
-                            std::string &legend, std::vector<int> &atomIds,
-                            std::vector<int> &bondIds) {
-  rj::Document doc;
-  doc.Parse(details.c_str());
-  if (!doc.IsObject()) return "Invalid JSON";
-
-  if (doc.HasMember("atoms")) {
-    if (!doc["atoms"].IsArray()) {
-      return "JSON doesn't contain 'atoms' field, or it is not an array";
-    }
-    for (const auto &molval : doc["atoms"].GetArray()) {
-      if (!molval.IsInt()) return ("Atom IDs should be integers");
-      atomIds.push_back(molval.GetInt());
-    }
-  }
-  if (doc.HasMember("bonds")) {
-    if (!doc["bonds"].IsArray()) {
-      return "JSON contain 'bonds' field, but it is not an array";
-    }
-    for (const auto &molval : doc["bonds"].GetArray()) {
-      if (!molval.IsInt()) return ("Bond IDs should be integers");
-      bondIds.push_back(molval.GetInt());
-    }
-  }
-
-  if (doc.HasMember("width")) {
-    if (!doc["width"].IsUint()) {
-      return "JSON contains 'width' field, but it is not an unsigned int";
-    }
-    width = doc["width"].GetUint();
-  }
-
-  if (doc.HasMember("height")) {
-    if (!doc["height"].IsUint()) {
-      return "JSON contains 'height' field, but it is not an unsigned int";
-    }
-    height = doc["height"].GetUint();
-  }
-
-  if (doc.HasMember("offsetx")) {
-    if (!doc["offsetx"].IsInt()) {
-      return "JSON contains 'offsetx' field, but it is not an int";
-    }
-    offsetx = doc["offsetx"].GetInt();
-  }
-
-  if (doc.HasMember("offsety")) {
-    if (!doc["offsety"].IsInt()) {
-      return "JSON contains 'offsety' field, but it is not an int";
-    }
-    offsety = doc["offsety"].GetInt();
-  }
-
-  if (doc.HasMember("legend")) {
-    if (!doc["legend"].IsString()) {
-      return "JSON contains 'legend' field, but it is not a string";
-    }
-    legend = doc["legend"].GetString();
-  }
-
-  return "";
-}
 
 namespace {
 RWMol *mol_from_input(const std::string &input) {
@@ -151,9 +84,9 @@ bool JSMol::get_substruct_match(const JSMol &q) const {
   }
 }
 
-std::string get_sub_fp(unsigned int minPath, unsigned int maxPath, unsigned int fpSize, unsigned int nBitsPerHash) const {
+std::string JSMol::get_sub_fp(unsigned int minPath, unsigned int maxPath, unsigned int fpSize, unsigned int nBitsPerHash) const {
   if (!d_mol) return "";
-  auto fp = Fingerprints::RDKFingerprintMol(*d_mol, minPath, maxPath, fpSize, nBitsPerHash)
+  auto fp = RDKit::RDKFingerprintMol(*d_mol, minPath, maxPath, fpSize, nBitsPerHash);
   std::string res = BitVectToText(*fp);
   delete fp;
   return res;
